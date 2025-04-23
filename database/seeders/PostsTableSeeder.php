8585755d71d43ed1a6c55d5ef42431b1607d4601
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-04-23 14:37:23 +0900

    add: Postに関するseedの定義

--- database/seeders/PostsTableSeeder.php
+++ database/seeders/PostsTableSeeder.php
@@ -4,14 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Post;
 
 class PostsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        Post::factory()->count(10)->create();
     }
 }
