ddaf3b16056084c3fbc604329ecc9350aefc1dbd
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-04-23 18:37:42 +0900

    add: createビューファイル作成

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -7,5 +7,34 @@
   </head>
   <body>
     <h1>投稿作成</h1>
+
+    @if ($errors->any())
+      <div>
+        <ul>
+          @foreach ($errors->all() as $error)
+            <li>
+              {{$error}}
+            </li>
+          @endforeach
+        </ul>
+      </div>
+    @endif
+
+    <form action="{{route('posts.store')}}" method="POST">
+      @csrf
+      <div>
+        <label for="title">タイトル</label>
+        <input type="text" name="title" value="{{old('title')}}">
+      </div>
+      <div>
+        <label for="content">
+          本文：
+        </label>
+        <textarea name="content">{{old('content')}}</textarea>
+      </div>
+      <button type="submit">投稿する</button>
+      <p><a href="{{route('posts.index')}}">
+        &laquo; 一覧に戻る
+      </a></p>
   </body>
 </html>
