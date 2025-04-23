b76bdc11f6f32d04cee8bcf639459c1e37260ca1
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-04-23 17:23:05 +0900

    add: indexページ作成

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,5 +7,18 @@
   </head>
   <body>
     <h1>投稿一覧</h1>
+    <ul>
+      @forelse ($posts as $post)
+        <li>
+          <a href="{{ route('posts.show', $post) }}">
+            {{$post -> title}}
+          </a>
+        </li>
+      @empty
+        <li>
+          投稿がありません。
+        </li>
+      @endforelse
+    </ul>
   </body>
 </html>
