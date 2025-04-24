cfdaaa04b52bde36501ca1fb5c63398382ad4c9f
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-04-24 21:39:13 +0900

    add: 削除ボタン追加

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -12,4 +12,9 @@
   <a href="{{route('posts.index')}}">&laquo; 一覧に戻る</a>
 </p>
 
+<form action="{{route('posts.destroy', $post)}}" method="POST" onsubmit="return confirm('本当に削除しますか？');">
+  @csrf @method('DELETE')
+  <button type="submit">削除する</button>
+</form>
+
 @endsection
