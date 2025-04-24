09176e424de6ec86923d191bdad973eda2e58660
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-04-24 20:41:50 +0900

    improve: showページのパーシャル化

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,19 +1,15 @@
-<!DOCTYPE html>
-<html lang="ja">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>投稿詳細</title>
-  </head>
-  <body>
-    <h1>
-      {{ $post->title }}
-    </h1>
-    <p>
-      {{ $post->content }}
-    </p>
-    <p>
-      <a href="{{route('posts.index')}}">&laquo; 一覧に戻る</a>
-    </p>
-  </body>
-</html>
+@extends('layouts.application')
+
+@section('content')
+
+<h1>
+  {{ $post->title }}
+</h1>
+<p>
+  {{ $post->content }}
+</p>
+<p>
+  <a href="{{route('posts.index')}}">&laquo; 一覧に戻る</a>
+</p>
+
+@endsection
