ad5291577d4cd1e1ab85015dd320dd439ede228e
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-04-24 21:19:34 +0900

    add: updateアクション追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -61,9 +61,18 @@ class PostController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Post $post)
     {
-        //
+        $validated = $request-> validate([
+            'title' => 'required|max:255',
+            'content' => 'required',
+        ]);
+
+        $post->update($validated);
+
+        session()->flash('success', '投稿を更新しました。');
+
+        return redirect()->route('posts.show', $post);
     }
 
     /**
