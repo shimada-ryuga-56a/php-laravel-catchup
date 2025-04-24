5398608a214b5139614de320514c6764e644e2d9
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-04-24 18:34:19 +0900

    fix: レイアウトに組み込まれるように修正

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,24 +1,17 @@
-<!DOCTYPE html>
-<html lang="ja">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>投稿一覧</title>
-  </head>
-  <body>
-    <h1>投稿一覧</h1>
-    <ul>
-      @forelse ($posts as $post)
-        <li>
-          <a href="{{ route('posts.show', $post) }}">
-            {{$post -> title}}
-          </a>
-        </li>
-      @empty
-        <li>
-          投稿がありません。
-        </li>
-      @endforelse
-    </ul>
-  </body>
-</html>
+@extends('layouts.application')
+
+@section('content')
+<h1>投稿一覧</h1>
+<ul>
+  @forelse ($posts as $post)
+    <li>
+      <a href="{{ route('posts.show', $post) }}">
+        {{$post -> title}}
+      </a>
+    </li>
+  @empty
+    <li>
+      投稿がありません。
+    </li>
+  @endforelse
+</ul>
