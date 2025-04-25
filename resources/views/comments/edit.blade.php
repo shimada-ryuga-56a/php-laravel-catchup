8d951ecbf4d09631ffcf31c1b387dc1e9b1af066
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-04-25 15:56:17 +0900

    add: エラーメッセージ表示

--- resources/views/comments/edit.blade.php
+++ resources/views/comments/edit.blade.php
@@ -1,3 +1,12 @@
 @extends('layouts.application')
 
 @section('title', 'コメント編集')
+
+@section('content')
+  <h1>コメント編集</h1>
+  @if ($errors->has('body'))
+    <div style="color:red">
+      {{$errors->first('body')}}
+    </div>
+  @endif
+@endsection
