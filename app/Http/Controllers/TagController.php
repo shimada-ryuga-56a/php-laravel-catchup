6b84b4737111f6ccca8b08b8a27f6c94e635d163
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-04-28 13:57:07 +0900

    add: indexアクション作成

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -12,7 +12,8 @@ class TagController extends Controller
      */
     public function index()
     {
-        //
+        $tags = Tag::all();
+        return view('tags.index', compact('tags'));
     }
 
     /**
