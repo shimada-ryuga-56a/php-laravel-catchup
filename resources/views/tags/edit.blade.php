d1137a3d2deb2beb76116a51041a7ddd3e6675e5
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-04-28 15:28:48 +0900

    add: エラー文表示

--- resources/views/tags/edit.blade.php
+++ resources/views/tags/edit.blade.php
@@ -1,4 +1,12 @@
 @extends('layouts.application')
 @section('title', 'タグ編集')
 @section('content')
+  <h1>タグ編集</h1>
+  <ul>
+    @foreach ($errors->all() as $error)
+      <li>
+        {{$error}}
+      </li>
+    @endforeach
+  </ul>
 @endsection
