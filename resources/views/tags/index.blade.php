0259bffb1975efda19157fd124bfe91a2e33fd61
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-04-28 14:04:16 +0900

    add: 一覧ページ作成

--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -1,3 +1,15 @@
-@extends('layout.application')
+@extends('layouts.application')
 @section('title', 'タグ一覧')
 @section('content')
+  <h1>タグ一覧</h1>
+  <p><a href="#">+ 新しいタグを作成</a></p>
+  <ul>
+    @forelse ($tags as $tag)
+      <li>
+        {{$tag->name}}
+      </li>
+    @empty
+      <li>まだタグがありません</li>
+    @endforelse
+  </ul>
+@endsection
