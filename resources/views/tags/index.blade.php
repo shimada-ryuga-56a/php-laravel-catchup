4c4885f61e677aabee64b4ae71c5b3f293274661
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-04-28 14:13:25 +0900

    add: リンク追加

--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'タグ一覧')
 @section('content')
   <h1>タグ一覧</h1>
-  <p><a href="#">+ 新しいタグを作成</a></p>
+  <p><a href="{{route('tags.create')}}">+ 新しいタグを作成</a></p>
   <ul>
     @forelse ($tags as $tag)
       <li>
