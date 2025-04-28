f6594f2607f365290cdfda7684f3d5afc53769d5
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-04-28 15:00:30 +0900

    add: storeアクション追加

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -27,10 +27,16 @@ class TagController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        //
-    }
+public function store(Request $request)
+{
+    $validated = $request->validate([
+        'name' => 'required|unique:tags,name|max:50',
+    ]);
+
+    Tag::create($validated);
+    session()->flash('success', 'タグを作成しました。');
+    return redirect()->route('tags.index');
+}
 
     /**
      * Display the specified resource.
