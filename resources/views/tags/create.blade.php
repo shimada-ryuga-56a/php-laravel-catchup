80bb8fda054bd576cf89d223a22b17328086914a
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-04-29 16:55:16 +0900

    add: パーシャル読み込み

--- resources/views/tags/create.blade.php
+++ resources/views/tags/create.blade.php
@@ -2,17 +2,7 @@
 @section('title', 'タグ作成')
 @section('content')
   <h1>タグ作成</h1>
-  @if ($errors->any())
-    <div>
-      <ul>
-        @foreach ($errors->all() as $error)
-          <li>
-            {{$error}}
-          </li>
-        @endforeach
-      </ul>
-    </div>
-  @endif
+  @include("partials.errors")
 
   <form action="{{route('tags.store')}}" method="POST">
     @csrf
