ca444d0b2ebe35e0b846d60cfe90ce2e5bbcbd91
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-04-29 16:26:59 +0900

    add: updateアクション実行時のバリデーション切り出し

---
+++ app/Http/Requests/UpdatePostRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdatePostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $postId = $this->route('post')?->id;
+
+        return [
+                'title'   => ['required', 'max:255', Rule::unique('posts', 'title')->ignore($postId)],
+                'content' => 'required',
+                'tags'    => 'array',
+                'tags.*'  => 'exists:tags,id',
+        ];
+    }
+}
