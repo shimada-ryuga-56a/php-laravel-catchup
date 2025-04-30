78cee2f7b40fe18de10d47ce185ffded2aff12f7
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-04-30 15:16:35 +0900

    add: tailwind読み込み

--- resources/views/layouts/application.blade.php
+++ resources/views/layouts/application.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
   </head>
-  <body>
+  <body class="font-heading bg-brand-light">
     @include('partials.flash')
     @yield('content')
   </body>
