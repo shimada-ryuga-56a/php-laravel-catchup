7d09fe558a7fd44eddd1ca74d575a84ad5cbc3fe
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-04-30 15:32:05 +0900

    add: ルーティング変更

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\PostController@index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
