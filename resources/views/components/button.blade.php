d09f1302c273aeab83171e2a16950e3793dbc690
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-05-01 22:38:47 +0900

    add: ボタンコンポーネント作成

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,11 @@
-<div>
-    <!-- Simplicity is the ultimate sophistication. - Leonardo da Vinci -->
-</div>
+@props([
+    'as' => 'button',
+])
+
+<{{ $as }}
+    {{ $attributes->merge([
+        'class' => 'px-4 py-2 bg-brand hover:bg-brand-dark text-white font-medium rounded flex justify-center'
+    ]) }}
+>
+    {{ $slot }}
+</{{ $as }}>
